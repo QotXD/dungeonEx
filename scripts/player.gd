2362def6f73c6c74008e4d330d18d7cf7bc79a53
sisamk@utu.fi <simo.karhu1@gmail.com>
2026-07-03 15:42:32 +0300

    health, player stats, death

--- scripts/player.gd
+++ scripts/player.gd
@@ -1,12 +1,62 @@
 extends CharacterBody3D
 
 
-const SPEED = 3.5
+const SPEED = 5.0
 const JUMP_VELOCITY = 4.0
 const FALL_GRAVITY_MULT = 1.2
 const LOW_JUMP_GRAVITY_MULT = 3.2
 
+const MAX_HEALTH = 20
+var health = MAX_HEALTH
 
+const DAMAGE = 3.5
+const ATTACK_SPEED = 1.0
+const CRITICAL_DAMAGE = 2.0
+const CRITICAL_CHANCE = 1.0
+
+
+
+
+#HEALTH
+func _ready() -> void:
+	update_health_ui()
+	$HealthBar.max_value = MAX_HEALTH
+	
+	
+func update_health_ui():
+	set_health_label()
+	set_health_bar()
+	
+func set_health_label() -> void:
+	$HealthLabel.text = "Health: %s" % health
+	
+func set_health_bar() -> void:
+	$HealthBar.value = health
+
+func _input(event: InputEvent) -> void:
+	if event.is_action_pressed("ui_accept"):
+		damage()
+		
+func damage() -> void:
+	health -= 1
+	if health < 0:
+		health = MAX_HEALTH
+	update_health_ui()
+	
+	if health <= 0:
+		die()
+
+
+
+#DEATH
+func die() -> void:
+	Engine.time_scale = 0.5
+	await get_tree().create_timer(2.0, false, false, true).timeout
+	Engine.time_scale = 1.0
+	get_tree().reload_current_scene()
+
+
+#MOVEMENT
 func _physics_process(delta: float) -> void:
 	# Add the gravity.
 	if not is_on_floor():
